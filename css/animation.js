813e95878ca59f9bc5d3fecb490ecf8fc6edcfe9
Haruki Adachi <h.adachi.cingroup2012@gmail.com>
2024-11-25 22:33:35 +0900

    HTML/CSS/jQueryを改善

--- css/animation.js
+++ css/animation.js
@@ -30,6 +30,30 @@
       }
   });
     
+  document.querySelectorAll('.topic').forEach(summary => {
+    summary.addEventListener('click', function () {
+      const panel = this.nextElementSibling;
+  
+     
+      document.querySelectorAll('.inner').forEach(inner => {
+        if (inner !== panel) {
+          inner.style.maxHeight = null;
+          inner.previousElementSibling.classList.remove('active');
+        }
+      });
+  
+    
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null; 
+        this.classList.remove('active');
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + 'px'; 
+        this.classList.add('active');
+      }
+    });
+  });
+  
+
     $(function () {
     $(window).scroll(function () {
         const wHeight = $(window).height();
@@ -43,38 +67,9 @@
     });
 });
 
-
-$(function() {
-  var scrollPos;//topからのスクロール位置
-  $('.open-btn1').click(function() { //ボタンをクリックしたら
-
-    $(this).toggleClass('active');//ボタン自身に activeクラスを付与し
-    $('.p-sp-nav-bg').toggleClass('bg-active');//背景にbg-activeクラスを付与
-    $('#sp-g-nav').toggleClass('panel-active');//ナビにpanel-activeクラスを付与
-    
-    if($(this).hasClass('active')){//もしボタンにactiveクラスが付いていれば
-      scrollPos = $(window).scrollTop();//topからのスクロール位置を取得
-      $('body').addClass('fixed').css({ top: -scrollPos });//背景固定
-      
-    }else{//もしボタンにactiveクラスが無ければ
-      $('body').removeClass('fixed').css({ top: 0 });//背景固定を解除
-      $(window).scrollTop(scrollPos);//元の位置までスクロール
-    }
-  });
-  $('.sp-nav-close').click(function() { //背景をクリックしたら
-    $('.open-btn1').removeClass('active');//ボタンからactiveクラスを剥奪
-    $('#sp-g-nav').removeClass('panel-active');//ナビからpanel-activeクラスを剥奪
-    $('.p-sp-nav-bg').removeClass('bg-active');//背景からbg-activeクラスを剥奪
-    $('body').removeClass('fixed').css({ top: 0 });//背景固定を解除
-    $(window).scrollTop(scrollPos);//元の位置までスクロール
-  });    
-  $(window).keyup(function(e){//キーをクリックして
-    if(e.key === "Escape"){//ESCキーだったら
-      $('.open-btn1').removeClass('active');//ボタンからactiveクラスを剥奪
-      $('#sp-g-nav').removeClass('panel-active');//ナビからpanel-activeクラスを剥奪
-      $('.p-sp-nav-bg').removeClass('bg-active');//背景からbg-activeクラスを剥奪
-      $('body').removeClass('fixed').css({ top: 0 });//背景固定を解除
-      $(window).scrollTop(scrollPos);//元の位置までスクロール
-    }
+$(function () {
+  $("#hamburger-icon").on("click", function () {
+    $(".menu-list").slideToggle("300"); 
   });
 });
+
