211cb23837ea58c11a0bef7c7814b626f8868b1c
Haruki Adachi <h.adachi.cingroup2012@gmail.com>
2024-11-26 22:10:37 +0900

    インデントの修正と余分な空白行の修正

--- animation.js
+++ animation.js
@@ -1,75 +1,65 @@
 
-    const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.swiper', {
     slidesPerView:2,
     spaceBetween:0,
-      loop: true,
-    
-      autoplay: {
+    loop: true,
+    autoplay: {
       delay: 3000,
-      },
+    },
 
-      pagination: {
-        el: '.swiper-pagination',
-      },
+    pagination: {
+      el: '.swiper-pagination',
+    },
 
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
 
-      scrollbar: {
-        el: '.swiper-scrollbar',
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+    breakpoints:{
+      780:{
+      slidesPerView:2,
       },
-      breakpoints:{
-        780:{
-          slidesPerView:2,
-        },
-        0:{
-          slidesPerView:1,
-        }
+      0:{
+      slidesPerView:1,
       }
-  });
+    }
+});
     
-  document.querySelectorAll('.topic').forEach(summary => {
-    summary.addEventListener('click', function () {
-      const panel = this.nextElementSibling;
+document.querySelectorAll('.topic').forEach(summary => {
+  summary.addEventListener('click', function () {
+  const panel = this.nextElementSibling;
+  document.querySelectorAll('.inner').forEach(inner => {
+  if (inner !== panel) {
+      inner.style.maxHeight = null;
+      inner.previousElementSibling.classList.remove('active');
+  }});
   
-     
-      document.querySelectorAll('.inner').forEach(inner => {
-        if (inner !== panel) {
-          inner.style.maxHeight = null;
-          inner.previousElementSibling.classList.remove('active');
-        }
-      });
-  
-    
-      if (panel.style.maxHeight) {
-        panel.style.maxHeight = null; 
-        this.classList.remove('active');
+  if (panel.style.maxHeight) {
+      panel.style.maxHeight = null; 
+      this.classList.remove('active');
       } else {
-        panel.style.maxHeight = panel.scrollHeight + 'px'; 
-        this.classList.add('active');
+      panel.style.maxHeight = panel.scrollHeight +'px'; 
+      this.classList.add('active');
       }
-    });
-  });
-  
-
-    $(function () {
-    $(window).scroll(function () {
-        const wHeight = $(window).height();
-        const wScroll = $(window).scrollTop();
-            $(".block").each(function () {
-                const bPosition = $(this).offset().top;
-            if (wScroll > bPosition - wHeight + 200) {
-                $(this).addClass("fadeIn");
-            }
-        });
     });
 });
 
-$(function () {
-  $("#hamburger-icon").on("click", function () {
-    $(".menu-list").slideToggle("300"); 
+$(window).scroll(function () {
+  const wHeight = $(window).height();
+  const wScroll = $(window).scrollTop();
+  $(".block").each(function () {
+  const bPosition = $(this).offset().top;
+  if (wScroll > bPosition - wHeight + 200) {
+  $(this).addClass("fadeIn");
+  }
   });
+});
+
+$("#hamburger-icon").on("click", function () {
+$(".menu-list").slideToggle("300"); 
 });
 
